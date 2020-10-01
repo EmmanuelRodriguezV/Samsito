5a808f6fe22f00adddbdf3ddf587990d2fa439db
emmanuel <ema-stuff@hotmail.com>
2020-10-01 22:05:45 +0200

    base preparation added
    Added bottom navigation  and page view controller

---
+++ flutter_app2/lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: '',),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+
+  int _currentIndex = 0;
+  PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox.expand(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() => {_currentIndex = index
+              
+            });
+          },
+          children: <Widget>[
+            Container(color: Colors.blueGrey,),
+            Container(color: Colors.blue,),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: _currentIndex,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        onItemSelected: (index) {
+          setState(() => _currentIndex = index);
+          _pageController.jumpToPage(index);
+        },
+        items: <BottomNavyBarItem>[
+          BottomNavyBarItem(
+              title: Text('Compras'),
+              icon: Icon(Icons.shopping_cart_rounded)
+          ),
+          BottomNavyBarItem(
+              title: Text('Productos'),
+              icon: Icon(Icons.fastfood_rounded)
+          )
+
+        ],
+      ),
+    );
+  }
+}
---
+++ flutter_app2/lib/test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+
+class Widgetsinstate extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+
+    );
+  }
+}
+
+class widgetconState extends StatefulWidget {
+  @override
+  _widgetconStateState createState() => _widgetconStateState();
+}
+
+class _widgetconStateState extends State<widgetconState> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
